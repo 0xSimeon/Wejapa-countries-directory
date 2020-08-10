e8047fe85d35bb6e619a06281701e7206b375de3
simeon4real <simeon.udoh45@gmail.com>
2020-08-10 01:39:17 +0100

    :zap: Loaded the components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<div id="app">
+		<div class="container">
+			<app-header></app-header>
+			<app-directory></app-directory>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Header from './components/Header.vue';
+import Directory from './components/Directory.vue';
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+		appDirectory: Directory,
+		appHeader: Header,
+	},
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+:root {
+	--color-primary: #00a896;
+	--color-secondary: #02c39a;
+	--color-tertiary: #f0f3bd;
+	--color-blue: #05668d;
+	--color-dave: #f0f3bd;
 }
+
+*,
+*::before,
+*::after {
+	margin: 0;
+	padding: 0;
+	box-sizing: inherit;
+}
+
+html {
+	box-sizing: border-box;
+	font-size: 56.25%;
+
+}
+
+#app {
+	padding: 1rem;
+	background: linear-gradient(to left top, var(--color-primary), var(--color-secondary));
+  height: 100%;
+  min-height: 100vh;
+	font-family: 'Roboto', sans-serif;
+}
+
+.container {
+  background: var(--color-tertiary);
+  width: 99%;
+
+  padding: 1.5rem;
+  border-radius: 4px;
+
+}
+
+
+
+@media only screen and (min-width: 37.5em) {
+  html {
+    font-size: 62.5%;
+  }
+
+  .container {
+    max-width: 70%;
+    margin: 2rem auto;
+  }
+}
+
+
 </style>
