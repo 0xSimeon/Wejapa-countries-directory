8c16dc5f0d20ef934e007760f9751cb0c4f92db2
simeon4real <simeon.udoh45@gmail.com>
2020-08-11 09:31:31 +0100

    :art: improved the Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,10 @@
 <template>
 	<div class="header">
-		<h1>Welcome to Wejapa Countries Directory 😃</h1>
+		<h1>
+			<a href="#" class="header__link"
+				><img class="header__logo" src="../assets/wejapa.png" alt="Wejapa logo" loading="lazy" /></a
+			> <span class="header__text">Countries</span>
+		</h1>
 	</div>
 </template>
 
@@ -8,11 +12,30 @@
 export default {};
 </script>
 
-<style>
+<style lang="scss">
 h1 {
 	font-size: 3.5rem;
-  margin-bottom: 1rem;
-  text-align: center;
+	margin-bottom: 1rem;
+	text-align: center;
 	font-family: 'Merriweather', serif;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+.header {
+	&__link {
+		text-decoration: none;
+	}
+
+	&__logo {
+		width: 50px;
+		height: 50px;
+	}
+	&__text {
+		background: linear-gradient(var(--color-primary), var(--color-secondary));
+		background-clip: text;
+		-webkit-text-fill-color: transparent;
+	}
 }
 </style>
